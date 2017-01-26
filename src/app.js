611f5eac59dc238e87021919ef8e2c59640c484b
Aaron Geiser <aaron.geiser@frogdesign.com>
2017-01-26 02:38:07 -0600

    Listen on pin 7

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-gpio.setup(14, gpio.DIR_IN, readInput);
+gpio.setup(7, gpio.DIR_IN, readInput);
 
 function readInput() {
     gpio.on('change', function(channel, value) {
         console.log('Channel ' + channel + ' value is now ' + value);
     });
-    gpio.setup(8, gpio.DIR_IN, gpio.EDGE_BOTH);
+    gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 }
 
 app.use('/', (req, res) => {
