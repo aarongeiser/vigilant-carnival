cac7988bba69f4c700a3aee810fa99e7db15fb3b
Aaron Geiser <aaron.geiser@frogdesign.com>
2017-01-31 17:25:11 -0600

    Stip down to basics

--- controller_service/src/app.js
+++ controller_service/src/app.js
@@ -1,12 +1,7 @@
-const http = require('http');
-const express = require('express');
 const gpio = require('rpi-gpio');
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+
+gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
 });
-
-gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
