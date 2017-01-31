be94a3a3519245c2838684b6013f9c03f01ae2aa
Aaron Geiser <aaron.geiser@frogdesign.com>
2017-01-31 17:35:46 -0600

    Pin 7

--- controller_service/src/app.js
+++ controller_service/src/app.js
@@ -1,7 +1,9 @@
 const gpio = require('rpi-gpio');
 
-gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
+console.log('Running...');
 
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
 });
+
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
