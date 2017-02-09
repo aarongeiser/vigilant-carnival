52e9dcb75be6c9b49dd15c69de3641141a35564f
Aaron Geiser <aarongeiser@gmail.com>
2017-02-08 22:58:03 -0600

    Emit source of event

--- controller_service/src/app.js
+++ controller_service/src/app.js
@@ -1,19 +1,18 @@
 // Controller Config
-require('dotenv').config();
-var button1 = process.env.CONTROLLER_NAME + "-1";
-var button2 = process.env.CONTROLLER_NAME + "-2";
-var pot = process.env.CONTROLLER_NAME + "-3";
-var stick_up = process.env.CONTROLLER_NAME + "-4";
-var stick_right = process.env.CONTROLLER_NAME + "-5";
-var stick_down = process.env.CONTROLLER_NAME + "-6";
-var stick_left = process.env.CONTROLLER_NAME + "-7";
+var button1 = process.env.CONTROLLER_NAME + "-button1";
+var button2 = process.env.CONTROLLER_NAME + "-button2";
+var pot = process.env.CONTROLLER_NAME + "-pot";
+var up = process.env.CONTROLLER_NAME + "-up";
+var right = process.env.CONTROLLER_NAME + "-right";
+var down = process.env.CONTROLLER_NAME + "-down";
+var left = process.env.CONTROLLER_NAME + "-left";
 
 // MCP ADC Setup
 var mcpadc = require('mcp-spi-adc');
 var buffer = 0.00;
 
 // Web Sockets Setup
-var socket = require('socket.io-client')(process.env.DISTRIBUTOR_LOCATION);  /* http:// required */
+var socket = require('socket.io-client')(process.env.SOCKET_ADDRESS);  /* http:// required */
 socket.on("event", function(data) { console.log(data); });
 socket.emit(button1, false);
 socket.emit(button2, false);
@@ -22,26 +21,31 @@ socket.emit(pot, buffer);
 // GPIO Setup
 var rpio = require('rpio');
 rpio.init({gpiomem: false}); /* Use /dev/mem */
-rpio.open(7, rpio.INPUT, rpio.PULL_UP);  /* button */
-rpio.open(8, rpio.INPUT, rpio.PULL_UP);  /* button */
-
-rpio.open(10, rpio.INPUT, rpio.PULL_UP);  /* joystick */
-rpio.open(11, rpio.INPUT, rpio.PULL_UP);  /* joystick */
-rpio.open(12, rpio.INPUT, rpio.PULL_UP);  /* joystick */
-rpio.open(13, rpio.INPUT, rpio.PULL_UP);  /* joystick */
+rpio.open(7, rpio.INPUT, rpio.PULL_UP);  /* button1 */
+rpio.open(8, rpio.INPUT, rpio.PULL_UP);  /* button2 */
+rpio.open(10, rpio.INPUT, rpio.PULL_UP);  /* stick_up */
+rpio.open(11, rpio.INPUT, rpio.PULL_UP);  /* stick_right */
+rpio.open(12, rpio.INPUT, rpio.PULL_UP);  /* stick_down */
+rpio.open(13, rpio.INPUT, rpio.PULL_UP);  /* stick_left */
 
 // Poll pin
 rpio.poll(7, function (pin) {
   var state = rpio.read(pin) ? 'released' : 'pressed';
   console.log('Button event on P%d (button currently %s)', pin, state);
-  socket.emit(button1, {'value':rpio.read(pin)});
+  socket.emit(button1, {
+      'source' : process.env.CONTROLLER_NAME,
+      'value':rpio.read(pin)
+  });
 });
 
 // Poll pin
 rpio.poll(8, function (pin) {
   var state = rpio.read(pin) ? 'released' : 'pressed';
   console.log('Button event on P%d (button currently %s)', pin, state);
-  socket.emit(button2, {'value':rpio.read(pin)});
+  socket.emit(button2, {
+    'source' : process.env.CONTROLLER_NAME,
+    'value':rpio.read(pin)
+  });
 });
 
 // Poll ADC Channel 0
@@ -51,7 +55,10 @@ var readpot = mcpadc.open(0, {speedHz: 1300000}, function (err) {
       var value = reading.value.toFixed(2);
       if (value != buffer) {
         console.log('Pot value: %d', value);
-        socket.emit(pot, {'value':value});
+        socket.emit(pot, {
+          'source' : process.env.CONTROLLER_NAME,
+          'value' : value
+        });
         buffer = value;
       }
     });
